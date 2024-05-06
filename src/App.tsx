bfba5a511c6a282b3f3efa7414b72c0e3dceff52
MatarSherman <matarsherman1@email.com>
2024-05-06 21:24:55 +0300

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import { Services } from './pages/HomePage/Services/Services';
 import { Clients } from './pages/HomePage/Clients/Clients';
 import { Team } from './pages/HomePage/Team/Team';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages/HomePage/HomePage';
 import { ExpertisePage } from './pages/ExpertisePage/ExpertisePage';
+import { HomePage } from './pages/HomePage/Homepage';
 
 export const App = () => {
   return (
